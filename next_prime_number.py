279ad44295216034dcdfb0ae8ddfb2693df61e72
Rombik <oleksa120699@gmail.com>
2019-01-02 16:39:43 +0200

    Change fucntion

--- next_prime_number.py
+++ next_prime_number.py
@@ -1,19 +1,28 @@
-def find_prime(n):
+from math import sqrt
+
+def find_prime():
+	n = 2
+	cause = False
 	while True:
-		for i in range(sqrt(a)):
+		#It's variable to check if we need to ask user to continue  
+		cause = False
+
+		for i in range(2,int(sqrt(n))+1):
 			if(n%i==0):
+				cause = True
+				n+=1
 				break
 		else:
-			return n
+			print(n)
 
+		if cause:
+			continue
+		#Ask to continue finding prime number
+		looking = input('Do you want to get another prime number? Please enter y or n: ')
+		if looking[0].lower() == 'y':
+			n+=1
+			continue
+		else:
+			break
 
-while True:
-	x = 2
-	print(find_prime(x))
-
-	#Ask to get another prime number
-	another = input('Do you want to get another prime number? Please enter y or n ':)
-	if another[0].lower() == 'y':
-		continue
-	else:
-		break
+find_prime()
